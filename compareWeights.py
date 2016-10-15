2c7a2fa3d3d597c7f86fc4dd05adc251e0ef1c9a
Adepu Ravi Sankar <cs14resch11001@iith.ac.in>
2016-10-15 10:03:17 +0530

    Update compareWeights.py

--- compareWeights.py
+++ compareWeights.py
@@ -1,6 +1,9 @@
 // Python code to compare weights from two different CNN's, to check the CNN's when trained with same random seed
+// net1 should be initialized to 0 else it goes into infinite loop
 import caffe
-net1 = caffe.Net('examples/mnist/lenet_train_test.prototxt','examples/mnist/lenet1_iter_1.caffemodel',caffe.TEST)
-net1.params['conv1'][0].data
-net2 = caffe.Net('examples/mnist/lenet_train_test.prototxt','examples/mnist/lenet2_iter_1.caffemodel',caffe.TEST)
-net1 == net2
+net1 = caffe.Net('examples/mnist/lenet_train_test.prototxt','examples/mnist/lenet1_iter_5.caffemodel',caffe.TEST)
+a=net1.params['conv1'][0].data
+net1=0
+net1 = caffe.Net('examples/mnist/lenet_train_test.prototxt','examples/mnist/lenet2_iter_5.caffemodel',caffe.TEST)
+b=net1.params['conv1'][0].data
+a==b
